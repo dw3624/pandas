298fca02cfc2a602f5d1661dea823d8033fa40d6
unknown <dw3624@gmail.com>
2021-01-15 22:26:50 +0900

    first commit

--- shiny/ui_data.R
+++
@@ -1,6 +0,0 @@
-tabItem_data<-tabItem('tab_data',
-                      fluidPage(
-                        titlePanel('DataTable'),
-                        id='datatab',
-                        DT::dataTableOutput('mytable'),style="overflow-y:scroll;overflow-x:scroll;"
-                      ))
